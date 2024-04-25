e900b0e4c769c0f5d80b37c3462dd3cdbf1e5199
Nilupul1 <167952469+Nilupul1@users.noreply.github.com>
2024-04-25 06:24:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94769482229', 'me', true],
+  ['94769482229', 'nilupul SL ', true], 
   [''] 
 ] //Number of owners
 
